8759c1f9abfdbad5146f99962beef2331d476315
Henna Singh <hennasingh04@gmail.com>
2024-07-01 14:21:35 +0000

    Add math logic to avoid negative results in subtraction

--- assets/js/index.js
+++ assets/js/index.js
@@ -110,8 +110,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = Math.max(operand1, operand2);
+    document.getElementById('operand2').textContent = Math.min(operand1, operand2);
     document.getElementById('operator').textContent = "-";
 }
 
